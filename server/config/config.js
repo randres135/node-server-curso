0d007d36f40c8a6accc99b702e3ebd6b7bc5ed9b
randres135 <randres135@gmail.com>
2020-11-28 18:36:20 -0600

    Variable de entorno prueba 1

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb+srv://mongo135:3bdQWgEaTr0CI6Ys@cluster0.bst0x.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB= urlDB;
